765bf2f194e86fac77c9cbb40d662f11df812792
Pierre BONNET <gogs@fake.local>
2020-02-21 13:52:47 +0100

    Update push.sh

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git remote add cleverapps
-https://$CLEVER_TOKEN:$CLEVER_SECRET@push.clever-cloud.com/app_YOUR_APPLICATION_ID.git
+https://$CLEVER_TOKEN:$CLEVER_SECRET@push.clever-cloud.com/app_Projet.git
 git --verbose --force push cleverapps master
